35900de5226ba9b2e244b91bbad84c566e45c4a7
Creo-KR <gjwlsdn0877@naver.com>
2021-07-20 10:44:41 +0900

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+FIVE
+
+Kotlin
+Spring Framework Boot
---
+++ src/main/kotlin/creo/games/five/server/controller/MainController.kt
@@ -0,0 +1,12 @@
+package creo.games.five.server.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class MainController {
+    @RequestMapping
+    @ResponseBody
+    fun main() : String = "main";
+}
